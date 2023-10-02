8a28c175efbab97e3b2f7714744296c3608e5ac6
Vigneshwaran <119487323+RMKEC111722203119@users.noreply.github.com>
2023-10-02 17:18:22 +0000

    in which day the members of the grp left the most

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-with open(r"_chat.txt", 'r') as fp:
+with open("_chat.txt", 'r') as fp:
     text = fp.readlines()
     lines = len(text)
-    pollcount =0
+    pollcount = 0
+    leave_events = {}
+    
     for eachline in text:
         if 'POLL' in eachline:
-            pollcount=pollcount+1
-    print('Total Number of lines:', lines)
-    print('Total polls: '+str(pollcount))
+            pollcount += 1
+        elif 'left the group' in eachline:
+            # Assuming the date is at the beginning of each line in a consistent format
+            date = eachline.split()[0]
+            if date in leave_events:
+                leave_events[date] += 1
+            else:
+                leave_events[date] = 1
     
+    print('Total Number of lines:', lines)
+    print('Total polls:', pollcount)
+    
+    # Find the date with the most leave events
+    if leave_events:
+        max_leave_date = max(leave_events, key=leave_events.get)
+        print('Date with the most leave events:', max_leave_date)
+        print('Number of leave events on that date:', leave_events[max_leave_date])
+    else:
+        print('No leave events found in the chat.')
